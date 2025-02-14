022ed2ad6593d9db697094b44e59709f7857f5d3
Slawomir Nowicki <slawomir_nowicki@wp.pl>
2025-02-14 01:02:38 +0100

    Switch to image crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use image::{GenericImageView, ImageReader, Pixel};
+
 fn main() {
-    println!("Hello, world!");
+    let img = ImageReader::open("test.bmp").unwrap().decode().unwrap();
+
+    let coordinates: Vec<[u32; 3]> = img
+        .pixels()
+        .map(|(x, y, pixel)| [x, y, pixel.to_luma().channels()[0] as u32])
+        .collect();
+
+    for coord in coordinates {
+        println!("x: {}, y: {}, value: {}", coord[0], coord[1], coord[2]);
+    }
 }
